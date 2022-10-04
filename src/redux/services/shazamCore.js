77224e67561e75032208ca4b8705ec33c60d48b2
Prathibha <prsthibha@gmail.com>
2022-10-04 11:22:42 +0530

    Add shazamCoreApi

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '609ce281ecmsh9080aa60a3cf2e8p1643dejsnfe52156dd3b9'
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
