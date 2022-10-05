c8e01b12a66c6f642914ee62ff71550246aa7f80
prathibha <prsthibha@gmail.com>
2022-10-05 11:19:26 +0530

    Add Search functionality

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -27,6 +27,9 @@ export const shazamCoreApi = createApi({
     getSongsByCountry: builder.query({
       query: (countryCode) => `/charts/country?country_code=${countryCode}`,
     }),
+    getSongsBySearch: builder.query({
+      query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
   }),
 });
 
@@ -37,4 +40,5 @@ export const {
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
   useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
 } = shazamCoreApi;
